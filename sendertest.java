7c74425276a8914fd274073d38e0a9b97ccbbe37
nickthename <spamedkid@gmail.com>
2014-09-22 14:51:26 -0400

    Rewrote client to include AWT gui, it works

--- sendertest.java
+++ sendertest.java
@@ -1,3 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.jnlp.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.Random;
 import java.io.*;
@@ -48,12 +53,14 @@ public class sendertest {
         RawSocket rs = new RawSocket();				
         rs.open(RawSocket.PF_INET, RawSocket.getProtocolByName("udp"));			
         rs.setIPHeaderInclude(true);
+	System.out.println("Socket created" + rs.isOpen());
         rs.write(dstAddress,buffer);
         rs.close();
     }
-    public static void main(String[] args) throws UnknownHostException, SocketException, IOException  {
+    public static boolean doTest() throws UnknownHostException, SocketException, IOException  {
+        System.out.println("Starting test");
         /* byte [] DEST={-58,38,23,53}; //198.38.23.53, hostname moon */
-        byte [] DEST={-58,38,18,97}; //198.38.23.53, hostname moon
+        byte [] DEST={-58,38,18,95}; //198.38.23.53, hostname moon
         byte [] SOURCE={-58,38,18,97}; //198.38.18.97
         byte [] SPOOF={19,20,18,91}; //198.38.18.97
         /* int DestAsInt = OctetConverter.octetsToInt(DEST);  */
@@ -79,16 +86,65 @@ public class sendertest {
                 ":f:" + RANDID + ":"
                 );
         String got;
-        Socket clientSocket = new Socket("198.38.18.97", 11112);
+        Socket clientSocket = new Socket("198.38.18.95", 11112);
         DataOutputStream outToServer = new DataOutputStream(
                 clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
                     clientSocket.getInputStream()));
-        outToServer.writeBytes(String.valueOf(RANDID));
+        outToServer.writeBytes(String.valueOf(RANDID) + ":");
+        try{
+            Thread.sleep(2000);
+        }
+        catch (Exception e){}
+        outToServer.writeBytes("Testing");
+        System.out.println("About to readline from server");
+        /* try{Thread.sleep(2000);} catch(Exception e){System.out.println(e);} */
         got = inFromServer.readLine();
-        System.out.println("Spoofing allowed: " + 
-                got);
+        System.out.println("got from server:" + got);
         clientSocket.close();
+        if(got.equals("True")){return true;}
+        else{return false;}
+    }
+
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException  {
+        final JFrame frame = new JFrame("BCP38 Checker thing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JLabel label = new JLabel();
+        Container content = frame.getContentPane();
+        content.add(label, BorderLayout.CENTER);
+        String message = "Jnln Hello Word";
+
+        label.setText(message);
+
+        JButton button = new JButton("I sure hope this works");
+
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                try{
+                    System.out.println("Button clicked");
+                    boolean canSpoof = doTest();
+                    if(canSpoof){
+                        System.out.println("Spoofing is possible!");
+                        label.setText("Spoofing possible");
+                        frame.pack();
+                        frame.show();
+                    }
+                    else{
+                        System.out.println("No spoofing is possible!");
+                        label.setText("No Spoofing!");
+                        frame.pack();
+                        frame.show();
+                    }
+                }
+                catch (Exception e){ System.err.println("caught:" + e); }
+            };
+        };
+
+        button.addActionListener(listener);
+        content.add(button, BorderLayout.SOUTH);
+        frame.pack();
+        frame.show();
+
 
     }
 }
