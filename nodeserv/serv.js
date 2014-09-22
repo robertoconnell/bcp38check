a70388013c542ec8f3888d8d67ebec4a3c849873
nickthename <spamedkid@gmail.com>
2014-09-22 14:47:33 -0400

    Improved readability, added comments

--- nodeserv/serv.js
+++ nodeserv/serv.js
@@ -1,6 +1,6 @@
 var UDPPORT = 11111;
 var TCPPORT = 11112;
-var HOST = '0.0.0.0';
+var HOST = '0.0.0.0'; //Should always be 0.0.0.0
 
 var dgram = require('dgram');
 var net = require('net');
@@ -8,7 +8,8 @@ var net = require('net');
 var udpserver = dgram.createSocket('udp4');
 var tcpserver = net.createServer();
 
-var sessions = new Object();
+var sessions = new Object(); //Associative array, format:  
+//sessions[session_id]=[packet 1 ip, packet 2 ip, packet 3 ip]
 
 udpserver.on('listening', function () {
     var address = udpserver.address();
@@ -16,15 +17,17 @@ udpserver.on('listening', function () {
 });
 
 udpserver.on('message', function (message, remote) {
-    var got = message.toString().split(":").slice(1);
+    var got = message.toString().split(":").slice(1); //Parts of the message are split by colons
+    //Slice removes front colon
+    //Got[0]=packet type Got[1]=session_id
     console.log(remote.address + ':' + remote.port +' - ' + got);
-    if(got[0]=="i") {
+    if(got[0]=="i") { //Inital, unspoofed, packet
         sessions[ got[1] ] = new Array(remote.address);
     }
-    if(got[0]=="s") {
+    if(got[0]=="s") { //Second, spoofed, packet
         sessions[ got[1] ].push(remote.address);
     }
-    if(got[0]=="f") {
+    if(got[0]=="f") { //Final, unspoofed, conclusions packet
         sessions[ got[1] ].push(remote.address);
     }
 
@@ -33,28 +36,30 @@ udpserver.on('message', function (message, remote) {
 tcpserver.on('connection', function(sock) {
 
     console.log(sessions);
-    var conid = "";
+    var session_id = "";
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
     sock.on('data', function(data) {
 
         data = data.toString();
-        if(data.toString().slice(-1)!=":"){conid+=data;}
-        else{
+        if(data.toString().slice(-1)!=":") {//The char ":" indicates the end of the session id
+            session_id+=data;
+        }
+        else {
 
-            conid += data.slice(0,-1);
-            console.log('DATA ' + sock.remoteAddress + ': ' + data + " " + conid);
+            session_id += data.slice(0,-1); //Remove the colon from the end of the session id
+            console.log('DATA ' + sock.remoteAddress + ': ' + data + " " + session_id);
 
-            if( !(sessions.hasOwnProperty(conid) && Array.isArray(sessions[conid])) ){
-                console.log("conid not in sessions");   
+            if( !(sessions.hasOwnProperty(session_id) && Array.isArray(sessions[session_id])) ) {
+                console.log("session_id not in sessions");   
                 sock.end();
                 return;
             }
-            console.log(sessions[conid]);
-            if(sessions[conid].length<3){
+            console.log(sessions[session_id]);
+            if(sessions[session_id].length<3) {
                 console.log(sock.remoteAddress + " " + "Less than 3 data points, no spoofing");
                 sock.write('False');
             }
-            else if(sessions[conid][1] == sessions[conid][0]){
+            else if(sessions[session_id][1] == sessions[session_id][0]) {
                 console.log(sock.remoteAddress + " " + "Spoofing did not occur");
                 sock.write('False');
             }
